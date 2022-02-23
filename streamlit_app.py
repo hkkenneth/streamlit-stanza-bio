bbebb439ec52b920847bfe8883a7ad37077c87d0
Kenneth Lui <hkkenneth@users.noreply.github.com>
2022-02-24 01:58:34 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,12 @@ stanza.download('en', package='mimic', processors={'ner': 'i2b2'})
 st.write('downloaded')
 nlp = stanza.Pipeline('en', package='mimic', processors={'ner': 'i2b2'})
 st.write('initialized')
-doc = nlp('Complete PSG with a digital sleep system using the international 10-20 electrode placement for recording EEG, EOG, EMG from chin, ECG, respiratory effort, oximetry, body position, airflow, snoring sound, pulse rate and limb movement channels.')
-st.write('Complete PSG with a digital sleep system using the international 10-20 electrode placement for recording EEG, EOG, EMG from chin, ECG, respiratory effort, oximetry, body position, airflow, snoring sound, pulse rate and limb movement channels.')
-df = pd.DataFrame([e.to_dict() for e in doc.entities])
-st.write(df)
+
+txt = st.text_area('Text to analyze', '''
+     Complete PSG with a digital sleep system using the international 10-20 electrode placement for recording EEG, EOG, EMG from chin, ECG, respiratory effort, oximetry, body position, airflow, snoring sound, pulse rate and limb movement channels.
+     ''')
+
+if st.button('Extract entities using Stanza'):
+  doc = nlp(txt)
+  df = pd.DataFrame([e.to_dict() for e in doc.entities])
+  st.write(df)
